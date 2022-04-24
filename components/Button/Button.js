3323175c5835b2ee049cff6b12ad2103e5b83946
Denny Cerritos portillo <dcerrito@c1r1s5.42madrid.com>
2022-04-24 17:18:25 +0200

    feat(Button.js): expect a className prop to re-use component

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,9 +1,10 @@
 import cx from "classnames";
 
-function Button({ children, isPrimary }) {
+function Button({ children, isPrimary, className }) {
   return (
     <div
       className={cx(
+        className,
         "flex items-center uppercase font-bold px-8 py-4 rounded-lg space-x-2",
         isPrimary
           ? "bg-lime-300 text-black"
