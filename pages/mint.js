41eacc32ee4323bc74d5996329c85e8ea10bc077
Denny Cerritos portillo <dcerrito@c1r1s5.42madrid.com>
2022-04-24 17:20:07 +0200

    chores(mint.js, send.js): add skeleton pages

---
+++ pages/mint.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+import TopNavigation from "@/components/TopNavigation/TopNavigation";
+
+function MintPage() {
+  return (
+    <Fragment>
+      <TopNavigation />
+      <b>MINT PAGE</b>
+    </Fragment>
+  );
+}
+
+export default MintPage;
---
+++ pages/send.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+import TopNavigation from "@/components/TopNavigation/TopNavigation";
+
+function SendPage() {
+  return (
+    <Fragment>
+      <TopNavigation />
+      <b>SEND PAGE</b>
+    </Fragment>
+  );
+}
+
+export default SendPage;
