f3baac62362e8be893e67263ef79f0b496eaccd2
Denny Cerritos portillo <dcerrito@c1r1s5.42madrid.com>
2022-04-24 18:42:35 +0200

    feat(Button): now function can expect an onClick prop

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,11 +1,14 @@
 import cx from "classnames";
 
-function Button({ children, isPrimary, className }) {
+function Button({ children, isPrimary, className, onClick }) {
   return (
     <div
+      tabIndex={0}
+      role="button"
+      onClick={onClick}
       className={cx(
         className,
-        "flex items-center uppercase font-bold px-8 py-4 rounded-lg space-x-2",
+        "flex cursor-pointer items-center uppercase font-bold px-8 py-4 rounded-lg space-x-2",
         isPrimary
           ? "bg-lime-300 text-black"
           : "text-white opacity-70 hover:opacity-100"
