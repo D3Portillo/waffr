fe5ed91d4f8dbf7c79a75359f826a4d1d3e748cf
Denny Cerritos portillo <dcerrito@c1r1s5.42madrid.com>
2022-04-24 16:33:27 +0200

    chores: add an unified Layout component && add favicon

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,18 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+const ICON_URL = "/favicon.png";
 function Layout({ children }) {
   return (
-    <div className="bg-black px-8 text-white min-h-screen">{children}</div>
+    <Fragment>
+      <Head>
+        <title>Waffr | Send, Mint, Enjoy</title>
+        <link rel="shortcut icon" href={ICON_URL} />
+        <link rel="apple-touch-icon" href={ICON_URL} />
+        <link rel="icon" type="image/png" href={ICON_URL} />
+      </Head>
+      <div className="bg-black px-8 text-white min-h-screen">{children}</div>
+    </Fragment>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Layout from "@/components/Layout";
 import TopNavigation from "@/components/TopNavigation/TopNavigation";
 
 export default function HomePage() {
-  return (
-    <Layout>
-      <TopNavigation />
-    </Layout>
-  );
+  return <TopNavigation />;
 }
