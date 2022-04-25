294656c5778452b1d7a4ccd027ceb7c8cf7066af
Denny Cerritos portillo <dcerrito@c1r1s6.42madrid.com>
2022-04-25 19:05:04 +0200

    feat(Input): add missing label, required props to component

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,8 +1,10 @@
-function Input({ value, onChange, label, placeholder }) {
+function Input({ value, onChange, label, placeholder, required }) {
   return (
     <div className="flex flex-col border border-zinc-600 w-full bg-white bg-opacity-5 rounded-lg px-8 py-4 text-sm space-y-2">
       <label className="text-zinc-300">{label}</label>
       <input
+        type="text"
+        required={required}
         placeholder={placeholder}
         value={value}
         onChange={({ target }) => onChange(target.value)}
@@ -13,6 +15,10 @@ function Input({ value, onChange, label, placeholder }) {
 }
 
 Input.defaultProps = {
+  value: undefined,
+  label: "",
+  placeholder: undefined,
+  required: undefined,
   onChange: () => null,
 };
 
