fd2e04189a324c33c49abb53d9954334c50e9e72
Denny Cerritos portillo <dcerrito@c1r1s6.42madrid.com>
2022-04-25 19:03:08 +0200

    feat(Layout.js): better position Toaster

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -13,7 +13,11 @@ function Layout({ children }) {
         <link rel="apple-touch-icon" href={ICON_URL} />
         <link rel="icon" type="image/png" href={ICON_URL} />
       </Head>
-      <Toaster />
+      <Toaster
+        containerStyle={{ top: "8rem" }}
+        position="top-right"
+        containerClassName="font-bold"
+      />
       <div className="bg-black px-4 lg:px-8 text-white min-h-screen">
         {children}
       </div>
