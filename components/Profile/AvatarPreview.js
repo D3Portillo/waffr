62a71306f2804b23bbb005b73b8a8f06cb4d4d76
Denny Cerritos portillo <dcerrito@c1r1s6.42madrid.com>
2022-04-25 19:11:04 +0200

    enh(Profile): better connection and error handling

--- components/Profile/AvatarPreview.js
+++ components/Profile/AvatarPreview.js
@@ -1,24 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
+import { MdClose } from "react-icons/md";
 
-function AvatarPreview({ account, formattedAccount, onDeactivate, avatarURL }) {
+function AvatarPreview({ formattedAccount, onDeactivate, avatarURL }) {
   return (
-    <div className="flex border-white border-t lg:border-0 w-full items-center justify-between relative pt-4 lg:p-0 flex-row-reverse">
+    <div className="flex border-white border-opacity-20 border-t lg:border-0 w-full items-center justify-between relative pt-4 lg:p-0 flex-row-reverse">
       <div
         tabIndex={0}
         role="button"
         onClick={onDeactivate}
-        className="lg:absolute h-16 flex bg-pink-600 font-bold text-white z-10 top-0 right-0 px-8 lg:h-full lg:opacity-0 hover:opacity-100 rounded-lg text-lg items-center justify-center"
+        className="lg:absolute h-16 flex bg-zinc-900 font-bold text-white z-10 top-0 right-0 px-8 lg:h-full lg:opacity-0 hover:opacity-100 rounded-lg text-lg items-center justify-center"
       >
         <span>DISCONNECT</span>
         <span className="hidden lg:inline mx-2">WALLET</span>
+        <span className="ml-2">
+          <MdClose />
+        </span>
       </div>
       <div className="flex items-center flex-row-reverse lg:flex-row">
         <div className="font-bold text-xl mx-2">{formattedAccount}</div>
         <div className="w-16 h-16 rounded-full border-2 border-lime-300">
           <img
+            role="figure"
             className="w-full h-full object-cover"
             layout="responsive"
-            alt={`Avatar image for account: ${account}`}
+            alt=""
             src={avatarURL}
           />
         </div>
--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -3,34 +3,39 @@ import { useEthers, useLookupAddress } from "@usedapp/core";
 
 import Button from "@/components/Button";
 import AvatarPreview from "./AvatarPreview";
+import useWalletConnect from "@/lib/hooks/useWalletConnect";
 import { formatWalletAddr } from "@/lib/utils/wallet";
 
 const DEFAULT_PROFILE_IMAGE = "/default_profile.png";
 
 function Profile() {
-  const [avatar, setAvatar] = useState(null);
-  const { activateBrowserWallet, account, deactivate, library } = useEthers();
-  const hasENS = useLookupAddress();
-  const possibleAccountAddr = hasENS || account;
-  const formattedAccount = formatWalletAddr(possibleAccountAddr, hasENS);
+  const [avatar, setAvatar] = useState(DEFAULT_PROFILE_IMAGE);
+  const { account, deactivate, library } = useEthers();
+  const ENSAdrr = useLookupAddress();
+  const possibleAccountAddr = ENSAdrr || account;
+  const formattedAccount = formatWalletAddr(possibleAccountAddr, ENSAdrr);
+  const handleConnect = useWalletConnect();
+
   useEffect(() => {
-    if (account) {
-      library.getAvatar(possibleAccountAddr).then(() => {
-        setAvatar(avatar || DEFAULT_PROFILE_IMAGE);
+    if (possibleAccountAddr) {
+      library.getAvatar(possibleAccountAddr).then((newAvatar) => {
+        setAvatar(newAvatar || DEFAULT_PROFILE_IMAGE);
       });
     }
   }, [possibleAccountAddr]);
+
+  const handleDisconnect = () => deactivate();
+
   return (
     <Fragment>
       {formattedAccount ? (
         <AvatarPreview
           formattedAccount={formattedAccount}
-          account={account}
           avatarURL={avatar}
-          onDeactivate={() => deactivate()}
+          onDeactivate={handleDisconnect}
         />
       ) : (
-        <Button onClick={() => activateBrowserWallet()} isPrimary>
+        <Button onClick={handleConnect} isPrimary>
           CONNECT TO METAMASK
         </Button>
       )}
