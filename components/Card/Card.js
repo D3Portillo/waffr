165fd669f8a52b79c4513081a87b4bde906fa361
Denny Cerritos portillo <dcerrito@c2r19s4.42madrid.com>
2022-04-27 09:47:46 +0200

    feat(Card.js): component now shows a loding state

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,13 +1,56 @@
-function Card({ children }) {
+import { Fragment, useMemo } from "react";
+import { useSendTransaction } from "@usedapp/core";
+
+import { IoArrowForward } from "react-icons/io5";
+import { RiLoader4Line } from "react-icons/ri";
+
+import Button from "@/components/Button";
+import CardTitle from "@/components/CardTitle";
+
+function LoadingState() {
+  const { state } = useSendTransaction();
+
+  const hash = useMemo(() => {
+    if (state && state.transaction) {
+      return state.transaction.hash;
+    }
+    return "";
+  }, [state]);
+  return (
+    <Fragment>
+      <CardTitle>
+        <div className="relative pr-2">
+          <RiLoader4Line className="animate-spin" />
+        </div>
+        <div>WORKING {"—"} MINING.</div>
+      </CardTitle>
+      <div className="h-2"></div>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block w-full relative"
+        href={`https://etherscan.io/tx/${hash}`}
+      >
+        <Button className="w-full bg-purple-500 py-6 text-xl opacity-90 pointer-events-none">
+          <div>VIEW TRANSACTION</div>
+          <IoArrowForward className="text-white text-2xl" />
+        </Button>
+      </a>
+    </Fragment>
+  );
+}
+
+function Card({ children, isLoading }) {
   return (
     <div className="bg-zinc-900 flex space-y-4 flex-col max-w-3xl mx-auto p-8 rounded-lg">
-      {children}
+      {isLoading ? <LoadingState /> : children}
     </div>
   );
 }
 
 Card.defaultProps = {
   children: null,
+  isLoading: false,
 };
 
 export default Card;
