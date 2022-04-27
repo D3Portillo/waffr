9e151cc8d772970326fd2740a9733b86c8d31e48
Denny Cerritos portillo <dcerrito@c1r17s4.42madrid.com>
2022-04-27 17:17:57 +0200

    enh(Mint.js): user can now mint WFL tokens

--- components/Mint/Mint.js
+++ components/Mint/Mint.js
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { useEthers } from "@usedapp/core";
+import { utils } from "ethers";
 
 import { IoArrowForward } from "react-icons/io5";
 import toast from "react-hot-toast";
 
+import { useMint } from "@/lib/hooks/useContract";
+import { addTokenToWallet } from "@/lib/utils/wallet";
+import { isMining, isSuccess } from "@/lib/utils/ether";
+import { preventDefaultEvent, withUnsignedInts } from "@/lib/utils/inputs";
 import useEtherProvider from "@/lib/hooks/useEtherProvider";
 import useWalletConnect from "@/lib/hooks/useWalletConnect";
-import { withFormattedBalance, preventDefaultEvent } from "@/lib/utils/inputs";
+import useCommonErrorHandler from "@/lib/hooks/useCommonErrorHandler";
 
 import StatesEmptyWallet from "@/components/StatesEmptyWallet";
 import Button from "@/components/Button";
@@ -15,21 +20,43 @@ import Card from "@/components/Card";
 import CardTitle from "@/components/CardTitle";
 
 function MintWall() {
+  const [isLoading, setIsLoading] = useState(false);
   const [amount, setAmount] = useState("");
   const [addrr, setAddrr] = useState("");
   const handleConnect = useWalletConnect();
   const { account } = useEthers();
-  function handleSend() {
+  const { send, state } = useMint();
+  useCommonErrorHandler(state);
+
+  function handleMint() {
     if (!account) return handleConnect();
     if (!addrr) return toast.error("YOU MUST PROVIDE AN ADDRESS");
     if (!amount) return toast.error("VALUE CANNOT BE ZERO");
-    // minting here
+    addTokenToWallet();
+    send(addrr, utils.parseEther(amount));
   }
+
+  useEffect(() => {
+    setIsLoading(false);
+    if (isMining(state)) {
+      setAddrr("");
+      setAmount("");
+      setIsLoading(true);
+    }
+    if (isSuccess(state)) {
+      toast("TOKENS MINTED SUCCESSFULLY");
+    }
+  }, [state]);
 
   return (
     <form onSubmit={preventDefaultEvent}>
-      <Card>
-        <CardTitle withAccountInfo>TOKEN MINTING</CardTitle>
+      <Card isLoading={isLoading}>
+        <CardTitle
+          withToken="0x9ed2135850920ba65566d010b947b49e88651675"
+          withAccountInfo
+        >
+          WFL TOKEN MINT
+        </CardTitle>
         <Input
           required
           value={addrr}
@@ -40,12 +67,12 @@ function MintWall() {
         <Input
           required
           value={amount}
-          onChange={(amount) => setAmount(withFormattedBalance(amount))}
+          onChange={(amount) => setAmount(withUnsignedInts(amount, false))}
           label="AMOUNT TO MINT"
-          placeholder="0.00"
+          placeholder="0"
         />
         <Button
-          onClick={handleSend}
+          onClick={handleMint}
           className="bg-purple-500 py-6 text-xl opacity-90"
         >
           <div>{account ? "MINT TOKENS" : "CONNECT & MINT"}</div>
