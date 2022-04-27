bb5a720a19a720d6a4e07b22e23129f7993e9b91
Denny Cerritos portillo <dcerrito@c2r19s4.42madrid.com>
2022-04-27 09:43:43 +0200

    fix(StatesEmptyWallet): update copy content

--- components/StatesEmptyWallet/StatesEmptyWallet.js
+++ components/StatesEmptyWallet/StatesEmptyWallet.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Image from "next/image";
 
 import Button from "@/components/Button";
@@ -6,18 +7,18 @@ import metaMaskFoxImage from "@/assets/metamask-fox.svg";
 function StatesEmptyWallet() {
   return (
     <div className="flex flex-col space-y-4 max-w-3xl mx-auto">
-      <h2 className="text-4xl">NO WALLET FOUND</h2>
+      <h2 className="text-4xl">WALLET NOT FOUND</h2>
 
       <p className="text-lg text-zinc-500">
-        To use this service please{" "}
         <a
           className="underline"
           target="_blank"
           rel="noopener noreferrer"
           href="https://metamask.io/download/"
         >
-          install MetaMask.
-        </a>
+          Please install MetaMask
+        </a>{" "}
+        to use this service.
         <br />
         MetaMask is a crypto wallet & gateway to blockchain apps Start exploring
         blockchain applications in seconds. Trusted by over 21 million users
@@ -30,7 +31,7 @@ function StatesEmptyWallet() {
         href="https://metamask.io/download/"
         rel="noopener noreferrer"
       >
-        <Button className="bg-zinc-900 py-2 space-x-6">
+        <Button className="bg-zinc-900 py-2 space-x-6 !opacity-100">
           <div className="w-16 h-16">
             <Image layout="responsive" src={metaMaskFoxImage} />
           </div>
