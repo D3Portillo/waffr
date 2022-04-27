ab8f2f204e9163fff9fe93e7c400160abf4d3d30
Denny Cerritos portillo <dcerrito@c2r19s4.42madrid.com>
2022-04-27 09:42:57 +0200

    feat(_app.js): include mainnet and rinkeby chains

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import { getDefaultProvider } from "ethers";
-import { Rinkeby, DAppProvider } from "@usedapp/core";
+import { Rinkeby, Mainnet, DAppProvider } from "@usedapp/core";
 
 import Layout from "@/components/Layout";
 
 import "../styles/globals.css";
 
+/** @type { import("@usedapp/core").FullConfig } */
 export const DAAP_CONFIG = {
-  readOnlyChainId: Rinkeby.chainId,
+  readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
+    [Mainnet.chainId]: getDefaultProvider("mainnet"),
     [Rinkeby.chainId]: getDefaultProvider("rinkeby"),
   },
 };
