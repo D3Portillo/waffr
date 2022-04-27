2e2833dd23d0e7ba02caedcc867714295f44fd61
Denny Cerritos portillo <dcerrito@c1r17s4.42madrid.com>
2022-04-27 18:51:22 +0200

    feat(TransactionList): add top 3 list component showcase

---
+++ components/TransactionList/TransactionList.js
@@ -0,0 +1,46 @@
+import { useTransactions } from "@usedapp/core";
+import { utils } from "ethers";
+
+import { FiArrowUpRight } from "react-icons/fi";
+import { relativeFormat } from "@/lib/utils/time";
+
+function TransactionList() {
+  const { transactions } = useTransactions();
+  if (transactions.length < 1 || !transactions) return null;
+  return (
+    <div className="max-w-3xl mx-auto flex-col space-y-4 mt-6 px-2">
+      <div className="text-zinc-400 font-bold text-sm">
+        LATEST 3 TRANSACTIONS
+      </div>
+      <div className="flex flex-col space-y-2">
+        {transactions.slice(0, 3).map((transaction) => {
+          return (
+            <a
+              key={transaction.transaction.hash}
+              href="https://etherscan.io/tx/"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="flex group text-sm justify-between border-b border-zinc-800 text-zinc-500 pb-2"
+            >
+              <div className="flex flex-col">
+                <strong className="text-zinc-300">
+                  {relativeFormat(transaction.submittedAt)}
+                </strong>
+                <div>
+                  <strong>TO:</strong> {transaction.transaction.to}
+                </div>
+                <div>
+                  <strong>VALUE:</strong>{" "}
+                  {utils.formatEther(transaction.transaction.value)}
+                </div>
+              </div>
+              <FiArrowUpRight className="text-2xl group-hover:text-white" />
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TransactionList;
