fd74b73c7b1257069b41c599c06aec0419f01313
Denny Cerritos portillo <dcerrito@c1r17s4.42madrid.com>
2022-04-28 23:25:59 +0200

    feat(Footer.js): flex-start elements on mobile

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,24 +4,24 @@ import Button from "@/components/Button";
 function Footer() {
   return (
     <div className="flex pb-6 pt-24 items-end lg:items-center">
-      <div className="flex lg:items-center flex-wrap space-y-2 lg:space-y-0 lg:space-x-4 flex-col lg:flex-row">
+      <div className="flex items-start lg:items-center flex-wrap space-y-2 lg:space-y-0 lg:space-x-4 flex-col lg:flex-row">
         <Button
-          isExternal
-          isLink
           href="https://github.com/D3Portillo/waffr/wiki"
           className="!p-0"
+          isExternal
+          isLink
         >
           ROADMAP
         </Button>
 
-        <Button isExternal isLink href="https://getwaffle.io/" className="!p-0">
+        <Button href="https://getwaffle.io/" className="!p-0" isExternal isLink>
           FRAMEWORK
         </Button>
         <Button
           href="https://www.d3portillo.me/"
+          className="!p-0"
           isExternal
           isLink
-          className="!p-0"
         >
           DEVELOPER
         </Button>
